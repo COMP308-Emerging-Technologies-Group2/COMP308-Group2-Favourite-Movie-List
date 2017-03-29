cba3d1fc2b3710da52554f80a167f23ba36fe05d
Aaron <aagavin@gmail.com>
2017-03-29 13:18:08 -0400

    moved search to http calls to its own service

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { NavController, NavParams } from 'ionic-angular';
+import { SearchProvider } from '../../providers/search';
 
 /*
   Generated class for the Search page.
@@ -10,43 +11,38 @@ import { NavController, NavParams } from 'ionic-angular';
 */
 @Component({
   selector: 'page-search',
-  templateUrl: 'search.html'
+  templateUrl: 'search.html',
+  providers: [SearchProvider]
 })
 export class SearchPage {
 
-  public searchResults;
+  public searchResults: Array<Object>;
 
-  constructor(private navCtrl: NavController, private navParams: NavParams, private http: Http) {
+  constructor(
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private http: Http,
+    private search: SearchProvider
+  ) {
     this.searchResults = new Array<Object>();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
-  }
-
-  public search(event): void {
-
+  ionViewDidLoad() { console.log('ionViewDidLoad SearchPage'); }
+  
+  public searchEvent(event): void {
     let query: string = event.target.value;
-
-    if (typeof query !== 'undefined') {
-      this.http.get(`https://v2.sg.media-imdb.com/suggests/${query[0]}/${query}.json`)
-        .subscribe(data => this._parseResponse(data['_body'], query))
-    }
-    else {
-      this.searchResults = [];
-    }
+    this.search.getSearchResults(query).then(value => {
+      console.log('********');
+      this.searchResults = value;
+      console.log('********');
+    })
   }
 
   public onCancel(): void { }
 
-  private _parseResponse(imdbResponse: string, query: string): void {
-    query = (query).split(' ').join('_')
-    let jsonResponse = imdbResponse.replace(`imdb$${query}(`, '')
-    jsonResponse = jsonResponse.substring(0, jsonResponse.length - 1);
-    jsonResponse = JSON.parse(jsonResponse)
-    jsonResponse = jsonResponse['d']
-    this.searchResults = jsonResponse;
-
+  public viewDetails(searchResult) {
+    // push to details page
+    // this.navCtrl.push
   }
 
 }
