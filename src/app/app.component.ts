9299a2592ee840f3a08bd5650eed2fea103fc621
LeontiBrechko <leontibrechko@gmail.com>
2017-03-30 12:13:22 -0400

    set up authentication, implemented core functions of auth-data provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import {Component, ViewChild} from "@angular/core";
+import {Nav, Platform} from "ionic-angular";
+import {StatusBar} from "@ionic-native/status-bar";
+import {SplashScreen} from "@ionic-native/splash-screen";
+import {Page1} from "../pages/page1/page1";
+import {Page2} from "../pages/page2/page2";
+import {AngularFire} from "angularfire2";
+import {LoginPage} from "../pages/login/login";
 
 
 @Component({
@@ -13,19 +14,31 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any;
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform,
+              public statusBar: StatusBar,
+              public splashScreen: SplashScreen,
+              public af: AngularFire) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      {title: 'Page One', component: Page1},
+      {title: 'Page Two', component: Page2}
     ];
 
+    const authObserver = af.auth.subscribe(user => {
+      if (user) {
+        this.rootPage = Page1;
+        authObserver.unsubscribe();
+      } else {
+        this.rootPage = LoginPage;
+        authObserver.unsubscribe();
+      }
+    });
   }
 
   initializeApp() {
--- src/providers/auth-data.ts
+++ src/providers/auth-data.ts
@@ -1,10 +1,33 @@
 import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
 import "rxjs/add/operator/map";
+import {AngularFire} from "angularfire2";
+import firebase from "firebase";
 
 @Injectable()
 export class AuthData {
-  constructor(public http: Http) {
-    console.log('Hello AuthData Provider');
+  fireAuth: any;
+
+  constructor(public af: AngularFire) {
+    af.auth.subscribe(user => {
+      if (user) {
+        this.fireAuth = user.auth;
+      }
+    });
+  }
+
+  login(email: string, password: string): firebase.Promise<any> {
+    return this.af.auth.login({email, password});
+  }
+
+  resetPassword(email: string): firebase.Promise<any> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+
+  logout(): firebase.Promise<any> {
+    return this.af.auth.logout();
+  }
+
+  register(email: string, password: string): firebase.Promise<any> {
+    return this.af.auth.createUser({email, password});
   }
 }
